fab17f5d21b2cda281f37866b6f82b931e5e058f
Rocío Martín <149075907+Rocio-fullstack@users.noreply.github.com>
2024-06-18 23:18:14 +0200

    cambio

--- src/front/js/component/KitchenCard.js
+++ src/front/js/component/KitchenCard.js
@@ -1,18 +1,34 @@
-
 import React, { useState, useEffect } from 'react';
 import '../../styles/kitchenCard.css';
-// import TableBarIcon from '@material-ui/icons/TableBar';
 import PeopleIcon from '@material-ui/icons/People';
-import { MdTableBar } from "react-icons/md";
+// import { MdTableBar } from "react-icons/md";
+import { MdOutlineTableRestaurant } from "react-icons/md";
 import { FaThumbtack } from "react-icons/fa6";
 import { LuTimer } from "react-icons/lu";
 
-
-
 const KitchenCard = ({ pedido }) => {
-
-
     const [time, setTime] = useState(60 * 60);
+    const [checkedItems, setCheckedItems] = useState({});
+
+    const allChecked = Object.values(checkedItems).every(item => item);
+    const handleCompleteClick = () => {
+        // Verifica si todos los elementos están marcados
+        const allChecked = Object.values(checkedItems).every(item => item);
+
+        if (allChecked) {
+            // Si todos los elementos están marcados, completa y elimina la orden
+            completeOrder(pedido.id);
+            deleteOrder(pedido.id);
+        } else {
+            // Si no todos los elementos están marcados, solo completa la orden
+            completeOrder(pedido.id);
+        }
+    };
+
+    const handleCheckChange = (event) => {
+        setCheckedItems({ ...checkedItems, [event.target.name]: event.target.checked });
+    };
+
     useEffect(() => {
         if (time > 0) {
             const timerId = setInterval(() => {
@@ -21,6 +37,7 @@ const KitchenCard = ({ pedido }) => {
             return () => clearInterval(timerId);
         }
     }, [time]);
+
     const formatTime = (seconds) => {
         const mins = Math.floor(seconds / 60);
         const secs = seconds % 60;
@@ -28,28 +45,40 @@ const KitchenCard = ({ pedido }) => {
     };
 
     return (
-        <>
-
-            <div className="kitchen-card">
-                <h2><FaThumbtack /> {pedido.id}</h2>
-                <p><MdTableBar /> {pedido.table}</p>
-                <p><PeopleIcon /> {pedido.comensales}</p>
-                {/* <p>Menú:</p> */}
-                <ul>
-                    {pedido.menu.map((item, itemIndex) => (
-                        <li key={itemIndex}>{item}</li>
-                    ))}
-                </ul>
-                <div style={{ textAlign: 'center', marginTop: '20%' }}><LuTimer /><h2>{formatTime(time)}</h2></div>
-                {/* <p className="timer">Tiempo de preparación {Math.floor(time / 60)} minutos {time % 60} segundos</p> */}
-                {/* <p className="timer">Tiempo de preparación: {pedido.preparationTime} minutos</p> */}
-
-
+        <div className="kitchen-card">
+            <div className="info-contenedor">
+                <div className="info-linea"><h2><FaThumbtack /> {pedido.id}</h2></div>
+                <div className="info-linea"><p><MdOutlineTableRestaurant /> {pedido.table}</p></div>
+                <div className="info-linea"> <p><PeopleIcon /> {pedido.comensales}</p></div>
             </div>
+            <ul>
+                {pedido.menu.map((item, itemIndex) => (
+                    <li key={itemIndex}>
+                        <input
+                            type="checkbox"
+                            name={item}
+                            checked={checkedItems[item] || false}
+                            onChange={handleCheckChange}
+                        />
+                        <span style={{
+                            textDecoration: checkedItems[item] ? 'line-through' : 'none',
+                            textDecorationColor: checkedItems[item] ? 'green' : 'initial'
+                        }}></span>
+                        {item}
+                    </li>
+                ))}
+            </ul>
 
-        </>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '10px', marginTop: '20%' }}>
+                <LuTimer /><h2>{formatTime(time)}</h2>
+            </div>
+            {/* <div style={{ textAlign: 'center', marginTop: '20%' }}>
+                <LuTimer /><h2>{formatTime(time)}</h2>
+            </div> */}
+            <button className="completar boton-tarjeta" onClick={handleCompleteClick}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" /></svg></button>
+            {pedido.completed && <button className="borrar" onClick={() => deleteOrder(pedido.id)}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" /></svg></button>}
+        </div>
     );
 };
 
 export default KitchenCard;
-
--- src/front/js/pages/kitchenview.js
+++ src/front/js/pages/kitchenview.js
@@ -7,28 +7,28 @@ import KitchenCard from "../component/kitchenCard";
 
 
 const KitchenView = () => {
-const {store, actions} = useContext(Context)
-const [pedidos, setPedidos] = useState([
-    { 
-        id: 1,
-        table: 2,
-        menu: [
-           "pulpo a la gallega", "gazpacho", "brownie"
-        ],
-        comensales: 3,
-        completed: false
-    },
-    { 
-        id: 2,
-        table: 3,
-        menu: [
-           "pizza napolitana", "sopa", "tarta de limón"
-        ],
-        comensales: 1,
-        completed: false
+  const { store, actions } = useContext(Context)
+  const [pedidos, setPedidos] = useState([
+    {
+      id: 1,
+      table: 2,
+      menu: [
+        "pulpo a la gallega", "gazpacho", "brownie"
+      ],
+      comensales: 3,
+      completed: false
     },
     {
-      id:3,
+      id: 2,
+      table: 3,
+      menu: [
+        "pizza napolitana", "sopa", "tarta de limón"
+      ],
+      comensales: 1,
+      completed: false
+    },
+    {
+      id: 3,
       table: 6,
       menu: [
         "ensalada", "raxo con patatas", "tiramisú"
@@ -37,7 +37,7 @@ const [pedidos, setPedidos] = useState([
       completed: false
     },
     {
-      id:4,
+      id: 4,
       table: 9,
       menu: [
         "revuelto de setas", "cachopo", "fresas con nata"
@@ -46,7 +46,7 @@ const [pedidos, setPedidos] = useState([
       completed: false
     },
     {
-      id:5,
+      id: 5,
       table: 5,
       menu: [
         "ensaladilla", "churrasco de cerdo", "coulant de chocolate"
@@ -55,7 +55,7 @@ const [pedidos, setPedidos] = useState([
       completed: false
     },
     {
-      id:6,
+      id: 6,
       table: 8,
       menu: [
         "sopa de verduras", "bacalao al pil pil", "tarta de queso"
@@ -64,7 +64,7 @@ const [pedidos, setPedidos] = useState([
       completed: false
     },
     {
-      id:7,
+      id: 7,
       table: 8,
       menu: [
         "sopa de verduras", "bacalao al pil pil", "tarta de queso"
@@ -73,7 +73,7 @@ const [pedidos, setPedidos] = useState([
       completed: false
     },
     {
-      id:8,
+      id: 8,
       table: 8,
       menu: [
         "sopa de verduras", "bacalao al pil pil", "tarta de queso"
@@ -82,27 +82,25 @@ const [pedidos, setPedidos] = useState([
       completed: false
     },
 
-]);
+  ]);
 
-const completeOrder = (id) => {
-    setPedidos(pedidos.map(pedido => 
-        pedido.id === id ? {...pedido, completed: true} : pedido
+  const completeOrder = (id) => {
+    setPedidos(pedidos.map(pedido =>
+      pedido.id === id ? { ...pedido, completed: true } : pedido
     ));
-}
+  }
 
-const deleteOrder = (id) => {
+  const deleteOrder = (id) => {
     setPedidos(pedidos.filter(pedido => pedido.id !== id));
-}
+  }
 
-return (
+  return (
     <div className="container1">
-      
+
       {pedidos.map(pedido => (
-        <div  className="pedidos" key={pedido.id}>
-          <KitchenCard pedido={pedido} /> 
-          {/* <p style={{ textDecoration: pedido.completed ? 'line-through' : 'none' }}>{pedido.menu}</p> */}
-          <button className="completar" onClick={() => completeOrder(pedido.id)}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg></button>
-          {pedido.completed && <button className="borrar" onClick={() => deleteOrder(pedido.id)}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></button>}
+        <div className="pedidos" key={pedido.id}>
+          <KitchenCard pedido={pedido} />
+
         </div>
       ))}
     </div>
