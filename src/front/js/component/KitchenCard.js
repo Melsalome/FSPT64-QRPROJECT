3b8d3e4ac66041720d8d811bc12fb6e25db32c0c
Rocío Martín <149075907+Rocio-fullstack@users.noreply.github.com>
2024-06-18 15:53:41 +0200

    nuevo

--- src/front/js/component/KitchenCard.js
+++ src/front/js/component/KitchenCard.js
@@ -3,40 +3,50 @@ import React, { useState, useEffect } from 'react';
 import '../../styles/kitchenCard.css';
 // import TableBarIcon from '@material-ui/icons/TableBar';
 import PeopleIcon from '@material-ui/icons/People';
+import { MdTableBar } from "react-icons/md";
+import { FaThumbtack } from "react-icons/fa6";
+import { LuTimer } from "react-icons/lu";
 
 
 
 const KitchenCard = ({ pedido }) => {
-    const [time, setTime] = useState(pedido.preparationTime * 60);
 
+
+    const [time, setTime] = useState(60 * 60);
     useEffect(() => {
-        const timer = setInterval(() => {
-            setTime(prevTime => prevTime - 1);
-        }, 1000);
-
-        return () => {
-            clearInterval(timer);
-        };
-    }, []);
+        if (time > 0) {
+            const timerId = setInterval(() => {
+                setTime((prevTime) => prevTime - 1);
+            }, 1000);
+            return () => clearInterval(timerId);
+        }
+    }, [time]);
+    const formatTime = (seconds) => {
+        const mins = Math.floor(seconds / 60);
+        const secs = seconds % 60;
+        return `${mins}:${secs < 10 ? 0 : ""}${secs}`;
+    };
 
     return (
         <>
-            
-                <div className="kitchen-card">
-                    <h2>Nº comanda {pedido.id}</h2>
-                    <p>Número de mesa {pedido.table}</p>
-                    <p><PeopleIcon /> {pedido.comensales}</p>
-                    {/* <p>Menú:</p> */}
-                    <ul>
-                        {pedido.menu.map((item, itemIndex) => (
-                            <li key={itemIndex}>{item}</li>
-                        ))}
-                    </ul>
-                    <p className="timer">Tiempo de preparación {Math.floor(time / 60)} minutos {time % 60} segundos</p>
-                    {/* <p className="timer">Tiempo de preparación: {pedido.preparationTime} minutos</p> */}
-                    
-                </div>
-           
+
+            <div className="kitchen-card">
+                <h2><FaThumbtack /> {pedido.id}</h2>
+                <p><MdTableBar /> {pedido.table}</p>
+                <p><PeopleIcon /> {pedido.comensales}</p>
+                {/* <p>Menú:</p> */}
+                <ul>
+                    {pedido.menu.map((item, itemIndex) => (
+                        <li key={itemIndex}>{item}</li>
+                    ))}
+                </ul>
+                <div style={{ textAlign: 'center', marginTop: '20%' }}><LuTimer /><h2>{formatTime(time)}</h2></div>
+                {/* <p className="timer">Tiempo de preparación {Math.floor(time / 60)} minutos {time % 60} segundos</p> */}
+                {/* <p className="timer">Tiempo de preparación: {pedido.preparationTime} minutos</p> */}
+
+
+            </div>
+
         </>
     );
 };
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
-import  {jwtDecode}  from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 import { Navigate } from "react-router-dom";
 
 import { Home } from "./pages/home";
@@ -16,26 +16,27 @@ import { Footer } from "./component/footer";
 import Signup from "./pages/signup";
 // import KitchenCard from "./component/kitchenCard";
 import Kitchenview from "./pages/kitchenview";
+import { KitchenAnhTu } from "./pages/kitchenviewAnhTu";
 
 const ProtectedRoute = ({ children, role }) => {
     const token = localStorage.getItem("token");
     if (!token) {
-      return <Navigate to="/app/login" />;
+        return <Navigate to="/app/login" />;
     }
     const decodedToken = jwtDecode(token);
     console.log(decodedToken.roles)
     if (role && decodedToken.roles !== role) {
-      return <Navigate to="/app/login" />;
+        return <Navigate to="/app/login" />;
     }
     return children;
-  };
+};
 //create your first component
 const Layout = () => {
     //the basename is used when your project is published in a subdirectory and not in the root of the domain
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
 
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
+    if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL />;
 
     return (
         <div>
@@ -43,13 +44,15 @@ const Layout = () => {
                 <ScrollToTop>
                     {/* <Navbar /> */}
                     <Routes>
-                        <Route element ={<Login/>} path="/app/login"/>
+                        <Route element={<Login />} path="/app/login" />
                         <Route element={<Signup />} path="/app/signup" />
-                        <Route element ={<Kitchenview/>} path="/app/kitchenview"/>
-                        
-   
-                    <Route element={<ProtectedRoute role="user"> <Home /> </ProtectedRoute>} path="app/home" />
-                        
+                        <Route element={<Kitchenview />} path="/app/kitchenview" />
+                        <Route element={<KitchenAnhTu />} path="/app/kitchenAnhTu" />
+
+
+
+                        <Route element={<ProtectedRoute role="user"> <Home /> </ProtectedRoute>} path="app/home" />
+
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     {/* <Footer /> */}
