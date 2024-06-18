9caffb678d672144636c90393831c99e32a222b7
Rocío Martín <149075907+Rocio-fullstack@users.noreply.github.com>
2024-06-18 13:47:44 +0200

    cambios2

--- src/front/js/component/KitchenCard.js
+++ src/front/js/component/KitchenCard.js
@@ -1,22 +1,39 @@
-import React from 'react';
+
+import React, { useState, useEffect } from 'react';
 import '../../styles/kitchenCard.css';
+// import TableBarIcon from '@material-ui/icons/TableBar';
+import PeopleIcon from '@material-ui/icons/People';
+
 
 
 const KitchenCard = ({ pedido }) => {
+    const [time, setTime] = useState(pedido.preparationTime * 60);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTime(prevTime => prevTime - 1);
+        }, 1000);
+
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+
     return (
         <>
             
                 <div className="kitchen-card">
-                    <h2>Comanda {pedido.id}</h2>
-                    <p>Número de mesa: {pedido.table}</p>
-                    <p>Número de comensales: {pedido.comensales}</p>
+                    <h2>Nº comanda {pedido.id}</h2>
+                    <p>Número de mesa {pedido.table}</p>
+                    <p><PeopleIcon /> {pedido.comensales}</p>
                     {/* <p>Menú:</p> */}
                     <ul>
                         {pedido.menu.map((item, itemIndex) => (
                             <li key={itemIndex}>{item}</li>
                         ))}
                     </ul>
-                    <p>Tiempo de preparación: {pedido.preparationTime} minutos</p>
+                    <p className="timer">Tiempo de preparación {Math.floor(time / 60)} minutos {time % 60} segundos</p>
+                    {/* <p className="timer">Tiempo de preparación: {pedido.preparationTime} minutos</p> */}
                     
                 </div>
            
--- src/front/js/pages/kitchenview.js
+++ src/front/js/pages/kitchenview.js
@@ -24,7 +24,7 @@ const [pedidos, setPedidos] = useState([
         menu: [
            "pizza napolitana", "sopa", "tarta de limón"
         ],
-        comensales: 4,
+        comensales: 1,
         completed: false
     },
     {
@@ -42,7 +42,7 @@ const [pedidos, setPedidos] = useState([
       menu: [
         "revuelto de setas", "cachopo", "fresas con nata"
       ],
-      comensales: 4,
+      comensales: 3,
       completed: false
     },
     {
@@ -51,6 +51,8 @@ const [pedidos, setPedidos] = useState([
       menu: [
         "ensaladilla", "churrasco de cerdo", "coulant de chocolate"
       ],
+      comensales: 4,
+      completed: false
     },
     {
       id:6,
@@ -58,20 +60,26 @@ const [pedidos, setPedidos] = useState([
       menu: [
         "sopa de verduras", "bacalao al pil pil", "tarta de queso"
       ],
+      comensales: 5,
+      completed: false
     },
     {
-      id:6,
+      id:7,
       table: 8,
       menu: [
         "sopa de verduras", "bacalao al pil pil", "tarta de queso"
       ],
+      comensales: 2,
+      completed: false
     },
     {
-      id:6,
+      id:8,
       table: 8,
       menu: [
         "sopa de verduras", "bacalao al pil pil", "tarta de queso"
       ],
+      comensales: 1,
+      completed: false
     },
 
 ]);
@@ -93,8 +101,8 @@ return (
         <div  className="pedidos" key={pedido.id}>
           <KitchenCard pedido={pedido} /> 
           {/* <p style={{ textDecoration: pedido.completed ? 'line-through' : 'none' }}>{pedido.menu}</p> */}
-          <button className="completar" onClick={() => completeOrder(pedido.id)}>Completar pedido</button>
-          {pedido.completed && <button className="borrar" onClick={() => deleteOrder(pedido.id)}>Borrar comanda</button>}
+          <button className="completar" onClick={() => completeOrder(pedido.id)}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg></button>
+          {pedido.completed && <button className="borrar" onClick={() => deleteOrder(pedido.id)}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></button>}
         </div>
       ))}
     </div>
