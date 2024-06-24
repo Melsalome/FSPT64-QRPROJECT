8b7db22ecd63bc801681e3f7f4ece1345b69e1f6
Rocío Martín <149075907+Rocio-fullstack@users.noreply.github.com>
2024-06-24 12:52:09 +0200

    merge

--- src/api/modelUser.py
+++ src/api/modelUser.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
 
     first_name = db.Column(db.String(255))
