1bc25d4dea962495922f27e0dd9930402bd170ff
Rocío Martín <149075907+Rocio-fullstack@users.noreply.github.com>
2024-06-24 12:55:21 +0200

    merge

--- src/api/modelUser.py
+++ src/api/modelUser.py
@@ -6,7 +6,6 @@ db = SQLAlchemy()
 class User(db.Model):
      __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-
     first_name = db.Column(db.String(255))
     last_name = db.Column(db.String(255))
     name_of_the_restaurant = db.Column(db.String(255))
