23d0f85b2633c1cdbe39141c07b55e8f2b977c8e
Melisa Navarro <151066758+Melsalome@users.noreply.github.com>
2024-07-06 16:57:20 +0200

    commit

--- src/api/wsgi.py
+++ src/api/wsgi.py
@@ -1,7 +1,11 @@
 # This file was created to run the application on heroku using gunicorn.
 # Read more about it here: https://devcenter.heroku.com/articles/python-gunicorn
 
+import os
+import sys
 from app import app as application
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src/api'))
 
 if __name__ == "__main__":
     application.run()
