c769d67f55f71c4395ab9f3c0459b02857bae63d
Melisa Navarro <151066758+Melsalome@users.noreply.github.com>
2024-07-06 22:15:21 +0200

    Cambio app.py y wsgi

--- src/app.py
+++ src/app.py
@@ -11,36 +11,82 @@ import base64
 import json
 
 load_dotenv()
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
-app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['ENV'] = os.getenv('FLASK_ENV')
-app.config['PORT'] = 3001
+db = SQLAlchemy()
+migrate = Migrate()
+jwt = JWTManager()
+cors = CORS()
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+# app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['ENV'] = os.getenv('FLASK_ENV')
+# app.config['PORT'] = 3001
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-jwt = JWTManager(app)
-CORS(app)
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
+# jwt = JWTManager(app)
+# CORS(app)
 
 
-# # Importar los modelos para que Alembic pueda detectarlos
+# # # Importar los modelos para que Alembic pueda detectarlos
 # from api.models import Table, Product, ProductTable, Client, Invoice, TableSession, Restaurant, Order, OrderItem
 
 
-from api.blueprints.table import table_bp
-from api.blueprints.product import product_bp
-from api.blueprints.client import client_bp
-from api.blueprints.productTable import productTable_bp
-from api.blueprints.sessions import sessions_bp
-from api.blueprints.auth import auth_bp
-from api.blueprints.restaurants import restaurants_bp
-from api.blueprints.generateqr import generateqr_bp
-app.register_blueprint(table_bp, url_prefix='/app')
-app.register_blueprint(product_bp, url_prefix='/app')
-app.register_blueprint(client_bp, url_prefix='/app')
-app.register_blueprint(productTable_bp, url_prefix='/app')
-app.register_blueprint(sessions_bp, url_prefix='/app')
-app.register_blueprint(auth_bp, url_prefix='/app')
-app.register_blueprint(restaurants_bp, url_prefix='/app')
-app.register_blueprint(generateqr_bp, url_prefix='/app')
+# from blueprints.table import table_bp
+# from api.blueprints.product import product_bp
+# from api.blueprints.client import client_bp
+# from api.blueprints.productTable import productTable_bp
+# from api.blueprints.sessions import sessions_bp
+# from api.blueprints.auth import auth_bp
+# from api.blueprints.restaurants import restaurants_bp
+# from api.blueprints.generateqr import generateqr_bp
+# app.register_blueprint(table_bp, url_prefix='/app')
+# app.register_blueprint(product_bp, url_prefix='/app')
+# app.register_blueprint(client_bp, url_prefix='/app')
+# app.register_blueprint(productTable_bp, url_prefix='/app')
+# app.register_blueprint(sessions_bp, url_prefix='/app')
+# app.register_blueprint(auth_bp, url_prefix='/app')
+# app.register_blueprint(restaurants_bp, url_prefix='/app')
+# app.register_blueprint(generateqr_bp, url_prefix='/app')
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['ENV'] = os.getenv('FLASK_ENV')
+    app.config['PORT'] = 3001
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+    cors.init_app(app)
+
+    # Registra los modelos para que Alembic pueda detectarlos
+    with app.app_context():
+        from api import models
+
+    # Importa y registra los blueprints
+    from api.blueprints.table import table_bp
+    from api.blueprints.product import product_bp
+    from api.blueprints.client import client_bp
+    from api.blueprints.productTable import productTable_bp
+    from api.blueprints.sessions import sessions_bp
+    from api.blueprints.auth import auth_bp
+    from api.blueprints.restaurants import restaurants_bp
+    from api.blueprints.generateqr import generateqr_bp
+
+    app.register_blueprint(table_bp, url_prefix='/app')
+    app.register_blueprint(product_bp, url_prefix='/app')
+    app.register_blueprint(client_bp, url_prefix='/app')
+    app.register_blueprint(productTable_bp, url_prefix='/app')
+    app.register_blueprint(sessions_bp, url_prefix='/app')
+    app.register_blueprint(auth_bp, url_prefix='/app')
+    app.register_blueprint(restaurants_bp, url_prefix='/app')
+    app.register_blueprint(generateqr_bp, url_prefix='/app')
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
--- src/wsgi.py
+++ src/wsgi.py
@@ -1,13 +1,25 @@
-# This file was created to run the application on heroku using gunicorn.
-# Read more about it here: https://devcenter.heroku.com/articles/python-gunicorn
+# # This file was created to run the application on heroku using gunicorn.
+# # Read more about it here: https://devcenter.heroku.com/articles/python-gunicorn
+
+# import os
+# import sys
+
+# from src.app import app as application
+
+# sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src/app'))
+
+
+# if __name__ == "__main__":
+#     application.run()
 
 import os
 import sys
 
-from src.app import app as application
+from app import create_app
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
 
+application = create_app()
 
 if __name__ == "__main__":
     application.run()
