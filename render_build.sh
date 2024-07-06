3f2f5427384afdd11768ffbab372fe0763f6e328
Melisa Navarro <151066758+Melsalome@users.noreply.github.com>
2024-07-06 22:29:01 +0200

    render_buil.sh

--- render_build.sh
+++ render_build.sh
@@ -1,7 +1,24 @@
+# #!/usr/bin/env bash
+# # exit on error
+# set -o errexit
+
+# npm install
+# npm run build
+
+# pipenv install
+
+# pipenv run upgrade
+
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
 
+# Asegurar permisos correctos
+find . -type f -exec chmod 644 {} \;
+find . -type d -exec chmod 755 {} \;
+chmod +x src/wsgi.py
+
+# Continuar con la instalación de dependencias y construcción
 npm install
 npm run build
 
