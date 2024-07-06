32494e100a70a9bcf0f4c00b99e3a6688b985546
Melisa Navarro <151066758+Melsalome@users.noreply.github.com>
2024-07-06 21:06:47 +0200

    Commit32

--- src/wsgi.py
+++ src/wsgi.py
@@ -4,10 +4,10 @@
 import os
 import sys
 
+from app import app as application
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
 
-from app import app as application
 
 if __name__ == "__main__":
     application.run()
