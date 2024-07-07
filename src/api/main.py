cafcf1bca8e2d0a8e31bd1247326601be252ab6e
Melisa Navarro <151066758+Melsalome@users.noreply.github.com>
2024-07-07 08:54:07 +0200

    Commit46

--- src/api/main.py
+++ src/api/main.py
@@ -2,12 +2,18 @@
 Punto de entrada de la aplicación
 """
 import os
-from flask import Flask, request, jsonify, url_for
-# from flask_cors import CORS
+from flask import Flask, request, jsonify, url_for, send_from_directory
+from flask_cors import CORS
 from .utils import APIException, generate_sitemap
 from src.app import app
 from api.admin import setup_admin
 from api.commands import setup_commands
+
+ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
+static_file_dir = os.path.join(os.path.dirname(
+    os.path.realpath(__file__)), '../public/')
+app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 setup_admin(app)
 setup_commands(app)
@@ -21,6 +27,14 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+@app.route('/<path:path>', methods=['GET'])
+def serve_any_other_file(path):
+    if not os.path.isfile(os.path.join(static_file_dir, path)):
+        path = 'index.html'
+    response = send_from_directory(static_file_dir, path)
+    response.cache_control.max_age = 0  # avoid cache memory
+    return response
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
 
--- src/app.py
+++ src/app.py
@@ -11,82 +11,45 @@ import base64
 import json
 
 load_dotenv()
-db = SQLAlchemy()
-migrate = Migrate()
-jwt = JWTManager()
-cors = CORS()
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
-# app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['ENV'] = os.getenv('FLASK_ENV')
-# app.config['PORT'] = 3001
-
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
-# jwt = JWTManager(app)
-# CORS(app)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['ENV'] = os.getenv('FLASK_ENV')
+app.config['PORT'] = 3001
 
 
-# # # Importar los modelos para que Alembic pueda detectarlos
-# from api.models import Table, Product, ProductTable, Client, Invoice, TableSession, Restaurant, Order, OrderItem
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+jwt = JWTManager(app)
+CORS(app)
 
 
-# from blueprints.table import table_bp
-# from api.blueprints.product import product_bp
-# from api.blueprints.client import client_bp
-# from api.blueprints.productTable import productTable_bp
-# from api.blueprints.sessions import sessions_bp
-# from api.blueprints.auth import auth_bp
-# from api.blueprints.restaurants import restaurants_bp
-# from api.blueprints.generateqr import generateqr_bp
-# app.register_blueprint(table_bp, url_prefix='/app')
-# app.register_blueprint(product_bp, url_prefix='/app')
-# app.register_blueprint(client_bp, url_prefix='/app')
-# app.register_blueprint(productTable_bp, url_prefix='/app')
-# app.register_blueprint(sessions_bp, url_prefix='/app')
-# app.register_blueprint(auth_bp, url_prefix='/app')
-# app.register_blueprint(restaurants_bp, url_prefix='/app')
-# app.register_blueprint(generateqr_bp, url_prefix='/app')
+# # Importar los modelos para que Alembic pueda detectarlos
+from models import Table, Product, ProductTable, Client, Invoice, TableSession, Restaurant, Order, OrderItem
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
-    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['ENV'] = os.getenv('FLASK_ENV')
-    app.config['PORT'] = 3001
 
-    db.init_app(app)
-    migrate.init_app(app, db)
-    jwt.init_app(app)
-    cors.init_app(app)
+from blueprints.table import table_bp
+from blueprints.product import product_bp
+from blueprints.client import client_bp
+from blueprints.productTable import productTable_bp
+from blueprints.sessions import sessions_bp
+from blueprints.auth import auth_bp
+from blueprints.restaurants import restaurants_bp
+from blueprints.generateqr import generateqr_bp
+app.register_blueprint(table_bp, url_prefix='/app')
+app.register_blueprint(product_bp, url_prefix='/app')
+app.register_blueprint(client_bp, url_prefix='/app')
+app.register_blueprint(productTable_bp, url_prefix='/app')
+app.register_blueprint(sessions_bp, url_prefix='/app')
+app.register_blueprint(auth_bp, url_prefix='/app')
+app.register_blueprint(restaurants_bp, url_prefix='/app')
+app.register_blueprint(generateqr_bp, url_prefix='/app')
+
 
     # Registra los modelos para que Alembic pueda detectarlos
-    with app.app_context():
-        from api import models
 
-    # Importa y registra los blueprints
-    from api.blueprints.table import table_bp
-    from api.blueprints.product import product_bp
-    from api.blueprints.client import client_bp
-    from api.blueprints.productTable import productTable_bp
-    from api.blueprints.sessions import sessions_bp
-    from api.blueprints.auth import auth_bp
-    from api.blueprints.restaurants import restaurants_bp
-    from api.blueprints.generateqr import generateqr_bp
+  
 
-    app.register_blueprint(table_bp, url_prefix='/app')
-    app.register_blueprint(product_bp, url_prefix='/app')
-    app.register_blueprint(client_bp, url_prefix='/app')
-    app.register_blueprint(productTable_bp, url_prefix='/app')
-    app.register_blueprint(sessions_bp, url_prefix='/app')
-    app.register_blueprint(auth_bp, url_prefix='/app')
-    app.register_blueprint(restaurants_bp, url_prefix='/app')
-    app.register_blueprint(generateqr_bp, url_prefix='/app')
+  
 
-    return app
-
-if __name__ == '__main__':
-    app = create_app()
-    app.run()
