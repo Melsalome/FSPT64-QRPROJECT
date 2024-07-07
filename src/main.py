ae95e10dcf9b59f539c7d1887839d3d6a4351f4a
Melisa Navarro <151066758+Melsalome@users.noreply.github.com>
2024-07-07 11:37:14 +0200

    todo al main

--- src/main.py
+++ src/main.py
@@ -8,12 +8,30 @@ from api.utils import APIException, generate_sitemap
 from app import app
 from api.admin import setup_admin
 from api.commands import setup_commands
+from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 
 # ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 # static_file_dir = os.path.join(os.path.dirname(
 #     os.path.realpath(__file__)), '../public/')
 # app = Flask(__name__)
 # app.url_map.strict_slashes = False
+load_dotenv()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['ENV'] = os.getenv('FLASK_ENV')
+app.config['PORT'] = 3001
+
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+jwt = JWTManager(app)
+CORS(app)
 
 setup_admin(app)
 setup_commands(app)
--- src/wsgi.py
+++ src/wsgi.py
@@ -1,6 +1,6 @@
 # This file was created to run the application on heroku using gunicorn.
 # Read more about it here: https://devcenter.heroku.com/articles/python-gunicorn
-from app import app as application
+from main import app
 
 if __name__ == "__main__":
-    application.run()
+    app.run()
